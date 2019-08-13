faf2aa6f38109cbabc9e7202c54dd5c785b32911
Jisepulveda <jiseulveda@miuandes.cl>
2019-08-13 11:53:28 -0300

    create app console 1.0

---
+++ Aplicacion 1.0/Aplicacion 1.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplicacion_1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
